27f6ff062debf0182fa4b304f09d29fcf270a2bc
Lee-Si-Yoon <siyoon@friendli.ai>
2024-06-10 01:23:40 +0900

    repo setup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+const packageJson = require('./package.json');
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  external: ['react', 'react-dom'],
+  plugins: [],
+};
