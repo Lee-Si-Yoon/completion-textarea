6f32655c40daf752a4ac923596eb9c70f62b45b9
Lee-Si-Yoon <siyoon@friendli.ai>
2024-06-12 07:06:46 +0900

    start planning

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # completion-textarea
+
+## Demos
+
+- [ ] vercel AI SDK
+- [ ] langchain
+
+## P1
+
+```mermaid
+flowchart TD
+
+initialize
+loading{"loading"}
+
+inputText["user inputs text"]
+inputAfterFetch{"user inputs"}
+
+requestData["request generated data"]
+
+userInputWithGenerated["renders user input + generated data"]
+userInputReplaced["user input = user input + generated data"]
+
+inputText --"with debounce"--> requestData
+
+requestData --> loading
+loading -->|Error| showError
+loading -->|Inputs something| initialize
+loading -->|No additional input for N ms| userInputWithGenerated
+
+userInputWithGenerated --> inputAfterFetch
+inputAfterFetch -->|Tab| userInputReplaced
+inputAfterFetch -->|Other| initialize
+
+userInputReplaced --> initialize
+```
+
+- [ ] implement above flow
+  - [ ] focus trap when tab is input
+- [ ] support debounce related props
+- [ ] support uncontrolled mode
+
+## P2
+
+- [ ] support autosize
+- [ ] support custom styling
+- [ ] support outer ref
+
+## P3
+
+- [ ] custom loading state
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,84 @@
 import React from 'react';
 
+type Nullish<T> = T | null | undefined;
+
+const noop = () => {};
+const delay = <T,>(time: number, value: T): Promise<T> =>
+  new Promise((res) => setTimeout(() => res(value), time));
+
+const getMockData = async () => {
+  const data = await delay(1000, 'done');
+
+  return data;
+};
+
 export const Text = () => {
-  return <i>test</i>;
+  const [value, setValue] = React.useState('');
+  const [generated, setGenerated] = React.useState('');
+
+  return (
+    <Inner
+      value={value}
+      setValue={setValue}
+      generated={generated}
+      setGenerated={setGenerated}
+      placeholder="type anything..."
+      onInput={async () => {
+        const data = await getMockData();
+
+        setGenerated(data);
+      }}
+    />
+  );
+};
+
+const Inner = ({
+  value,
+  setValue,
+  generated,
+  setGenerated,
+  ...props
+}: {
+  /* TODO: support uncontrolled mode */
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  generated?: Nullish<string>;
+  setGenerated?: React.Dispatch<React.SetStateAction<string>>;
+} & React.TextareaHTMLAttributes<HTMLTextAreaElement>) => {
+  return (
+    <div style={{ position: 'relative' }}>
+      <textarea
+        value={value + ' ' + generated}
+        onChange={noop}
+        style={{
+          border: '1px solid red',
+          position: 'absolute',
+          color: '#999',
+          width: '100%',
+          resize: 'none',
+        }}
+      />
+      <textarea
+        {...props}
+        value={value}
+        onInput={(e) => {
+          setValue(e.currentTarget.value);
+          props.onInput && props.onInput(e);
+        }}
+        onKeyDown={(e) => {
+          /* TODO: focus trap */
+          if (e.key === 'Tab') {
+            setValue(value + ' ' + generated);
+            setGenerated && setGenerated('');
+          }
+        }}
+        style={{
+          position: 'absolute',
+          backgroundColor: 'transparent',
+          width: '100%',
+          resize: 'none',
+        }}
+      />
+    </div>
+  );
 };
